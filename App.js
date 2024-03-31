2f819d839fe84d6c8147c37676568f9f7805c344
Tao Zhan <130600637+SoulCold404@users.noreply.github.com>
2024-03-31 16:15:07 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ToDoList from './app/ToDoList';
+import ToDoForm from './app/ToDoForm';
+
+export default function App() {
+  return (
+    <>
+    <ToDoList />
+    <ToDoForm />
+    </>
+
+  );
+}
